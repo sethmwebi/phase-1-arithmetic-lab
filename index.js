2aa1a93a55dbc3826f0d111b7126df2bc3657f3f
Seth Mwebi <sethmwebi27@gmail.com>
2024-06-25 14:27:07 +0300

    writes code for phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const numbers = [10, 6, 5, 20];
+const max = Math.max(...numbers);
+
